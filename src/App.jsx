fa891cd1823b18b54288419ad143362e62ac5da4
YOGESH <111962172+Yogeshrajput24179@users.noreply.github.com>
2025-07-06 23:07:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar.jsx'
-import Sidebar from './components/sidebar/Sidebar.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Add from './pages/Add/Add.jsx'
-import List from './pages/List/List.jsx'
-import Order from './pages/Orders/Order.jsx'
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react';
+import Navbar from './components/Navbar/Navbar.jsx';
+import Sidebar from './components/sidebar/Sidebar.jsx';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Add from './pages/Add/Add.jsx';
+import List from './pages/List/List.jsx';
+import Order from './pages/Orders/Order.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  // Use live backend URL here
   const url = "https://adminpannel-bharatcrafts.onrender.com";
 
   return (
@@ -21,6 +20,7 @@ const App = () => {
         <div className='app-content'>
           <Sidebar />
           <Routes>
+            <Route path='/' element={<Navigate to="/add" />} />
             <Route path='/add' element={<Add url={url} />} />
             <Route path='/List' element={<List url={url} />} />
             <Route path='/Orders' element={<Order url={url} />} />
@@ -28,7 +28,7 @@ const App = () => {
         </div>
       </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
